28d183c6b68beff187b308e7204f962e3018fe04
dator <dennizyilmaz1903@gmail.com>
2023-10-10 19:46:03 +0200

    lol

--- src/models/ReservationChambreModel.ts
+++ src/models/ReservationChambreModel.ts
@@ -41,7 +41,7 @@ const ReservationChambreScheme = new Schema<IReservationChambreDetails>({
 	starters: { type: Array },
 	mains: { type: Array },
 	desserts: { type: Array },
-	drinks: { type: Array },
+	drinks: { type: String },
 	comment: { type: String },
 	other: { type: String },
 });
