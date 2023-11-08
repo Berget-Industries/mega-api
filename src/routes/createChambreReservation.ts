162e360d66e5b9cec40a63d14fed80522f69db3c
willebergh <willebergh@outlook.com>
2023-11-08 20:22:19 +0100

    fixade alla error meddelanden till ett stort meddelande

--- src/routes/createChambreReservation.ts
+++ src/routes/createChambreReservation.ts
@@ -35,30 +35,44 @@ async function createChambreReservation(ctx: Context) {
 		})
 			.filter(([k, v]) => v == null || v === "")
 			.map(([k, v]) => k);
-
-		if (missingInformation.length > 0) {
-			const body = getMissingInformationErrorMessage(missingInformation.toString());
-			ctx.response.status = 200;
-			ctx.response.body = body;
-			console.log(body);
-			return;
-		}
+		const missingInformationMessage =
+			missingInformation.length > 0
+				? getMissingInformationErrorMessage(missingInformation.toString())
+				: "det saknas ingen information";
 
 		const brokenRules = checkChambreBookingRules(input);
-		if (brokenRules.length > 0) {
-			const body = getBrokenRulesErrorMessage(brokenRules);
-			ctx.response.status = 200;
-			ctx.response.body = body;
-			console.log(body);
-			return;
-		}
+		const brokenRulesMessage =
+			brokenRules.length > 0 ? getBrokenRulesErrorMessage(brokenRules) : "alla regler följs";
 
-		const isAvailable = await checkAvailableDates({ date, time });
-		if (!isAvailable) {
-			const body = getNotAvailableErrorMessage();
+		const isDateAndTimeRulesBroken = brokenRules.filter(
+			(_) => _.inputKey === "time" || "date"
+		).length;
+		console.log(isDateAndTimeRulesBroken);
+		const isAvailable =
+			date && time && isDateAndTimeRulesBroken === 0
+				? await checkAvailableDates({ date, time })
+				: null;
+		const isAvailableMessage =
+			isAvailable === null
+				? "tid och eller datum saknas"
+				: isAvailable === false
+				? getNotAvailableErrorMessage()
+				: "önskad tid går att boka";
+
+		const responseBody = `
+====
+${JSON.stringify(missingInformationMessage)}
+====
+${JSON.stringify(brokenRulesMessage)}
+====
+${JSON.stringify(isAvailableMessage)}
+====
+`;
+
+		if (missingInformation.length > 0 || brokenRules.length > 0 || !isAvailable) {
 			ctx.response.status = 200;
-			ctx.response.body = body;
-			console.log(body);
+			ctx.response.body = responseBody;
+			console.log(responseBody);
 			return;
 		}
 
