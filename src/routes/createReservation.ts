355b9f7c2d4f81822e682799d31f74a75497bd59
willebergh <willebergh@outlook.com>
2023-11-19 15:36:20 +0100

    changed logic to work with both chambre and regualr bookings

--- src/routes/createReservation.ts
+++ src/routes/createReservation.ts
@@ -5,8 +5,12 @@ import {
 	getCreateReservationSuccessMessage,
 	getCreateReservationErrorMessage,
 	getMissingInformationErrorMessage,
+	getNotAvailableErrorMessage,
+	getBrokenRulesErrorMessage,
 } from "../utils/errorMessages.ts";
 
+import { checkAvailableDates, addReservationToDate } from "../utils/availableDates.ts";
+import { checkChambreBookingRules, checkNormalBookingRules } from "../utils/checkBookingRules.ts";
 import { IReservationDetails } from "../models/ReservationModel.ts";
 
 async function createReservation(ctx: Context) {
@@ -33,19 +37,67 @@ async function createReservation(ctx: Context) {
 			comment,
 		};
 
-		const isNull = Object.entries(input)
-			.filter(([v]) => v == null || v === "")
-			.map(([k]) => k);
+		const missingInformation = Object.entries({
+			name,
+			email,
+			date,
+			time,
+			numberOfGuests,
+			phone,
+		})
+			.filter(([k, v]) => v == null || v === "")
+			.map(([k, v]) => k);
+		const missingInformationMessage =
+			missingInformation.length > 0
+				? getMissingInformationErrorMessage(missingInformation.toString())
+				: "det saknas ingen information";
 
-		if (isNull.length > 0) {
-			const body = getMissingInformationErrorMessage(isNull.toString());
+		const brokenRules = chambre
+			? checkChambreBookingRules(input)
+			: checkNormalBookingRules(input);
+		const brokenRulesMessage =
+			brokenRules.length > 0 ? getBrokenRulesErrorMessage(brokenRules) : "alla regler följs";
+
+		const isDateAndTimeRulesBroken = brokenRules.filter(
+			(_) => _.inputKey === "time" || "date"
+		).length;
+
+		const isAvailable =
+			date && time && isDateAndTimeRulesBroken === 0
+				? chambre
+					? await checkAvailableDates({ date, time })
+					: true
+				: null;
+		const isAvailableMessage =
+			isAvailable === null
+				? "tid och eller datum saknas"
+				: isAvailable === false
+				? getNotAvailableErrorMessage()
+				: "önskad tid går att boka";
+
+		const responseBody = `
+====
+${JSON.stringify(missingInformationMessage)}
+====
+${JSON.stringify(brokenRulesMessage)}
+====
+${JSON.stringify(isAvailableMessage)}
+====
+`;
+
+		if (missingInformation.length > 0 || brokenRules.length > 0 || !isAvailable) {
 			ctx.response.status = 200;
-			ctx.response.body = body;
-			console.log(body);
+			ctx.response.body = responseBody;
+			console.log(responseBody);
 			return;
 		}
 
 		const reservationDetails = await Reservation.create(input);
+		await addReservationToDate({
+			date,
+			time,
+			reservationId: reservationDetails._id.toString(),
+		});
 
 		const body = getCreateReservationSuccessMessage(reservationDetails);
 		ctx.response.status = 200;
