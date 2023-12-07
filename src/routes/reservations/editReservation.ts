663edceb92b0c60bb16f6194b1fd8db41dccfe23
Denniz Yilmaz <112471865+dennizyilmaz99@users.noreply.github.com>
2023-12-07 15:56:25 +0100

    fixed rules for edit

--- src/routes/reservations/editReservation.ts
+++ src/routes/reservations/editReservation.ts
@@ -8,17 +8,27 @@ import {
 	getEditReservationNoChangeMessage,
 } from "../../utils/errorMessages.ts";
 import convertToUTC from "../../utils/convertToUTC.ts";
-
 import { handleResponseError, handleResponseSuccess } from "../../utils/contextHandler.ts";
+import { checkAvailableDates } from "../../utils/availableDates.ts";
+import {
+	checkChambreBookingRules,
+	checkNormalBookingRules,
+} from "../../utils/checkBookingRules.ts";
+import {
+	getBrokenRulesErrorMessage,
+	getMissingInformationErrorMessage,
+	getNotAvailableErrorMessage,
+} from "../../utils/errorMessages.ts";
 
 const router = new Router();
 
 router.post("/reservation/edit", async (ctx: Context) => {
 	try {
-		const { _id, name, email, date, time, numberOfGuests, phone, conversationId } =
+		const { _id, chambre, name, email, date, time, numberOfGuests, phone, conversationId } =
 			await ctx.request.body().value;
 
 		const input = {
+			chambre,
 			name,
 			email,
 			date: convertToUTC(date, time),
@@ -36,6 +46,58 @@ router.post("/reservation/edit", async (ctx: Context) => {
 		if (Object.keys(updateData).length === 0) {
 			const body = getEditReservationNoChangeMessage();
 			handleResponseSuccess(ctx, body);
+			return;
+		}
+
+		const missingInformation = Object.entries({
+			name,
+			email,
+			date,
+			time,
+			numberOfGuests,
+			phone,
+		})
+			.filter(([k, v]) => v == null || v === "")
+			.map(([k, v]) => k);
+		const missingInformationMessage =
+			missingInformation.length > 0
+				? getMissingInformationErrorMessage(missingInformation.toString())
+				: "det saknas ingen information";
+
+		const brokenRules = chambre
+			? checkChambreBookingRules({ ...input, time })
+			: checkNormalBookingRules({ ...input, time });
+		const brokenRulesMessage =
+			brokenRules.length > 0 ? getBrokenRulesErrorMessage(brokenRules) : "alla regler följs";
+
+		const isDateAndTimeRulesBroken = brokenRules.filter(
+			(_) => _.inputKey === "time" || "date"
+		).length;
+		const isAvailable =
+			date && time && isDateAndTimeRulesBroken === 0
+				? chambre
+					? await checkAvailableDates({ date, time })
+					: true
+				: null;
+		const isAvailableMessage =
+			isAvailable === null
+				? "tid och eller datum saknas"
+				: isAvailable === false
+				? getNotAvailableErrorMessage()
+				: "önskad tid går att boka";
+
+		const responseBody = `
+====
+${JSON.stringify(missingInformationMessage)}
+====
+${JSON.stringify(brokenRulesMessage)}
+====
+${JSON.stringify(isAvailableMessage)}
+====
+`;
+
+		if (missingInformation.length > 0 || brokenRules.length > 0 || !isAvailable) {
+			handleResponseSuccess(ctx, responseBody);
 			return;
 		}
 
