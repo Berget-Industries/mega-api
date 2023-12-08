1321d45c74db96b1888577a127fe1a333b9b43c2
willebergh <willebergh@outlook.com>
2023-12-08 21:29:43 +0100

    removed unused file

--- src/routes/auth/index.ts
+++ src/routes/auth/index.ts
@@ -3,14 +3,13 @@ import login from "./login.ts";
 import logout from "./logout.ts";
 import me from "./me.ts";
 import resetPasswoord from "./resetPassword.ts";
-import ai from "./ai.ts";
 
 import { Router } from "https://deno.land/x/oak@v12.6.1/mod.ts";
 
 const router = new Router();
 const basePath = "/auth";
 
-const routes = [changePassword, login, logout, me, resetPasswoord, ai];
+const routes = [changePassword, login, logout, me, resetPasswoord];
 
 routes.forEach((_) => {
 	router.use(basePath, _.routes());
