7c8ec48a84b6db2a6574fe8d7f1d706e1a2463ac
Denniz Yilmaz <112471865+dennizyilmaz99@users.noreply.github.com>
2023-12-11 21:22:07 +0100

    its going to be a conflict because i removed the branch that i already pushed to so willy this is for you

--- src/models/Reservation.ts
+++ src/models/Reservation.ts
@@ -2,29 +2,25 @@ import { model, Schema } from "npm:mongoose";
 
 export interface IReservationDetails {
 	chambre: boolean;
-	name: string;
-	email: string;
 	date: Date;
 	numberOfGuests: number;
-	phone: string;
 	comment: string;
 	menu: object | undefined;
 	conversations: [{ type: Schema.Types.ObjectId; ref: "Conversation" }];
 	organization: { type: Schema.Types.ObjectId; ref: "Organization" };
+	contact: { type: Schema.Types.ObjectId; ref: "Contact" };
 }
 
 export default model<IReservationDetails>(
 	"ReservationDetails",
 	new Schema<IReservationDetails>({
 		chambre: { type: Boolean },
-		name: { type: String },
-		email: { type: String },
 		date: { type: Date },
 		numberOfGuests: { type: Number },
 		comment: { type: String },
-		phone: { type: String },
 		menu: { type: Object },
 		conversations: [{ type: Schema.Types.ObjectId, ref: "Conversation" }],
 		organization: { type: Schema.Types.ObjectId, ref: "Organization" },
+		contact: { type: Schema.Types.ObjectId, ref: "Contact" },
 	})
 );
