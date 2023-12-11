c4512c588973ca69d1e7859b38650027a89ebb76
willebergh <willebergh@outlook.com>
2023-12-11 20:09:36 +0100

    removed deleted functions

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -6,13 +6,11 @@ import reservations from "./ai/reservations/index.ts";
 import getConverstaion from "./getConversation.ts";
 import addSelectedMenu from "./addSelectedMenu.ts";
 import getConversations from "./getConversations.ts";
-import addMessageHistory from "./addMessageHistory.ts";
 import aiFunctions from "./ai/index.ts";
 
 const router = new Router();
 const routes = [
 	reservations,
-	addMessageHistory,
 	addSelectedMenu,
 	auth,
 	admin,
