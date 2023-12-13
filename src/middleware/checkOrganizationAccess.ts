ea061e62ea5fa4a5fb54b9d648c23b92b250dfaa
willebergh <willebergh@outlook.com>
2023-12-14 00:55:35 +0100

    finally fixed this middleware. now it works. had to compare everything as strings insted.

--- src/middleware/checkOrganizationAccess.ts
+++ src/middleware/checkOrganizationAccess.ts
@@ -1,22 +1,29 @@
 import { Context, Next } from "https://deno.land/x/oak/mod.ts";
 import { Organization } from "../models/index.ts";
+import { Types } from "npm:mongoose";
 
 export default async function checkOrganizationAccess(ctx: Context, next: Next) {
-	// const userId = ctx.state.session.user._id;
-	const userOrganizations = ctx.state.session.user.organizations;
+	const userId = ctx.state.session.user.id.toString();
+	const userOrganizations = ctx.state.session.user.organizations.map((_: Types.ObjectId) =>
+		_.toString()
+	);
+
 	const requestedOrganization: string | null = ctx.request.hasBody
 		? (await ctx.request.body().value).organization
 		: ctx.request.url.searchParams.get("organization");
 
 	const organizationDoc = await Organization.findById(requestedOrganization);
+	if (!organizationDoc) {
+		return;
+	}
 
-	if (
-		userOrganizations &&
-		requestedOrganization &&
-		organizationDoc &&
-		userOrganizations.includes(requestedOrganization)
-	) {
-		ctx.state.organization = requestedOrganization;
+	const organizationUsers = organizationDoc.users.map((_: Types.ObjectId) => _.toString());
+
+	const isUserInOrg = organizationUsers.includes(userId);
+	const isOrgInUser = userOrganizations.includes(requestedOrganization);
+
+	if (isUserInOrg && isOrgInUser) {
+		ctx.state.organization = organizationDoc._id;
 		await next();
 	} else {
 		ctx.response.status = 404;
