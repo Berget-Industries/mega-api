2e60517bfa542a9855670f9d71c2ce5f765bcd64
willebergh <willebergh@outlook.com>
2023-12-13 01:03:28 +0100

    changed response.body och la till support for both post and get requests

--- src/middleware/checkOrganizationAccess.ts
+++ src/middleware/checkOrganizationAccess.ts
@@ -1,19 +1,28 @@
 import { Context, Next } from "https://deno.land/x/oak/mod.ts";
+import { Organization } from "../models/index.ts";
 
 export default async function checkOrganizationAccess(ctx: Context, next: Next) {
+	// const userId = ctx.state.session.user._id;
 	const userOrganizations = ctx.state.session.user.organizations;
-	const body = await ctx.request.body().value;
-	const requestedOrganization = body.organization;
+	const requestedOrganization: string | null = ctx.request.hasBody
+		? (await ctx.request.body().value).organization
+		: ctx.request.url.searchParams.get("organization");
+
+	const organizationDoc = await Organization.findById(requestedOrganization);
 
 	if (
 		userOrganizations &&
 		requestedOrganization &&
+		organizationDoc &&
 		userOrganizations.includes(requestedOrganization)
 	) {
 		await next();
 	} else {
-		ctx.response.status = 401;
-		ctx.response.body = { message: "Unauthorized" };
+		ctx.response.status = 404;
+		ctx.response.body = {
+			status: "error",
+			message: "Unauthorized",
+		};
 		return;
 	}
 }
