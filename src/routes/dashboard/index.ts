44c44b286492541d6ffd835afedc157977d9fb0d
willebergh <willebergh@outlook.com>
2023-12-14 00:53:52 +0100

    removed middlewere here since it did not work

--- src/routes/dashboard/index.ts
+++ src/routes/dashboard/index.ts
@@ -2,15 +2,14 @@ import getMessages from "./getMessages.ts";
 import getConverstaion from "./getConversation.ts";
 import getConversations from "./getConversations.ts";
 import { Router } from "https://deno.land/x/oak@v12.6.1/mod.ts";
-import checkOrganizationAccess from "../../middleware/checkOrganizationAccess.ts";
 
 const router = new Router();
 const basePath = "/dashboard";
 const routes = [getConversations, getConverstaion, getMessages];
 
 routes.forEach((_) => {
-	router.use(basePath, checkOrganizationAccess, _.routes());
-	router.use(basePath, checkOrganizationAccess, _.allowedMethods());
+	router.use(basePath, _.routes());
+	router.use(basePath, _.allowedMethods());
 });
 
 export default router;
