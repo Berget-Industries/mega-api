4c76d36131ae3ba1faeea754f32a36d7bb81f44f
willebergh <willebergh@outlook.com>
2023-12-16 14:08:08 +0100

    removed comment

--- src/routes/ai/addMessageHistory.ts
+++ src/routes/ai/addMessageHistory.ts
@@ -6,82 +6,77 @@ import { handleResponseError, handleResponseSuccess } from "../../utils/contextH
 import { Conversation, Message, Contact, Organization } from "../../models/index.ts";
 
 const router = new Router();
-router.post(
-	"/addMessageHistory",
-	//authenticationMiddleware,
-	aiAuthenticationMiddleware,
-	async (ctx: Context) => {
-		try {
-			const { conversation, contactEmail, contactName, createdAt, input, llmOutput } =
-				await ctx.request.body().value;
-			const organization = ctx.state.organization;
+router.post("/addMessageHistory", aiAuthenticationMiddleware, async (ctx: Context) => {
+	try {
+		const { conversation, contactEmail, contactName, createdAt, input, llmOutput } =
+			await ctx.request.body().value;
+		const organization = ctx.state.organization;
 
-			let contactDoc = await Contact.findOne({ email: contactEmail });
-			if (!contactDoc) {
-				contactDoc = await Contact.create({
-					email: contactEmail,
-					name: contactName,
-				});
-			}
-
-			let conversationDoc = await Conversation.findById(conversation);
-			if (!conversationDoc) {
-				conversationDoc = await Conversation.create({
-					_id: conversation,
-					messages: [],
-					lastActivity: createdAt,
-				});
-			}
-
-			const messageDoc = await Message.create({
-				conversation: conversationDoc._id,
-				organization,
-				contact: contactDoc._id,
-				createdAt,
-				input,
-				llmOutput,
-			});
-
-			conversationDoc.organization = organization;
-			conversationDoc.contact = contactDoc._id;
-			conversationDoc.lastActivity = createdAt;
-			conversationDoc.messages = [...conversationDoc.messages, messageDoc._id];
-			await conversationDoc.save();
-
-			await Organization.updateOne(
-				{ _id: organization },
-				{
-					$addToSet: {
-						conversations: conversationDoc._id,
-						messages: messageDoc._id,
-					},
-				}
-			);
-
-			const newConv = await Conversation.findById({ _id: conversation })
-				.populate("messages")
-				.exec();
-			if (!newConv) throw "asd";
-			handleResponseSuccess(ctx, {
-				status: "invalid-id",
-				message: "Kunde inte hitta konversationen. ID:et är ogiltigt.",
-				conversation: newConv.toJSON(),
-			});
-		} catch (error) {
-			console.error(error);
-			if (error instanceof mongoose.Error.CastError) {
-				handleResponseError(ctx, {
-					status: "error",
-					message: "Nånting gick fel.",
-				});
-				return;
-			}
-			handleResponseError(ctx, {
-				status: "internal-error",
-				message: "Tekniskt fel.",
+		let contactDoc = await Contact.findOne({ email: contactEmail });
+		if (!contactDoc) {
+			contactDoc = await Contact.create({
+				email: contactEmail,
+				name: contactName,
 			});
 		}
+
+		let conversationDoc = await Conversation.findById(conversation);
+		if (!conversationDoc) {
+			conversationDoc = await Conversation.create({
+				_id: conversation,
+				messages: [],
+				lastActivity: createdAt,
+			});
+		}
+
+		const messageDoc = await Message.create({
+			conversation: conversationDoc._id,
+			organization,
+			contact: contactDoc._id,
+			createdAt,
+			input,
+			llmOutput,
+		});
+
+		conversationDoc.organization = organization;
+		conversationDoc.contact = contactDoc._id;
+		conversationDoc.lastActivity = createdAt;
+		conversationDoc.messages = [...conversationDoc.messages, messageDoc._id];
+		await conversationDoc.save();
+
+		await Organization.updateOne(
+			{ _id: organization },
+			{
+				$addToSet: {
+					conversations: conversationDoc._id,
+					messages: messageDoc._id,
+				},
+			}
+		);
+
+		const newConv = await Conversation.findById({ _id: conversation })
+			.populate("messages")
+			.exec();
+		if (!newConv) throw "asd";
+		handleResponseSuccess(ctx, {
+			status: "invalid-id",
+			message: "Kunde inte hitta konversationen. ID:et är ogiltigt.",
+			conversation: newConv.toJSON(),
+		});
+	} catch (error) {
+		console.error(error);
+		if (error instanceof mongoose.Error.CastError) {
+			handleResponseError(ctx, {
+				status: "error",
+				message: "Nånting gick fel.",
+			});
+			return;
+		}
+		handleResponseError(ctx, {
+			status: "internal-error",
+			message: "Tekniskt fel.",
+		});
 	}
-);
+});
 
 export default router;
