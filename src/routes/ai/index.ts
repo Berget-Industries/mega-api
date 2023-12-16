3f97464c497de6de73b85b803339b311211d8524
willebergh <willebergh@outlook.com>
2023-12-16 15:20:36 +0100

    removed import from removed file

--- src/routes/ai/index.ts
+++ src/routes/ai/index.ts
@@ -1,11 +1,10 @@
 import { Router } from "https://deno.land/x/oak@v12.6.1/mod.ts";
-import updateContact from "./updateContact.ts";
 import reservationRoutes from "./reservations/index.ts";
 import addMessageHistory from "./addMessageHistory.ts";
 
 const router = new Router();
 const basePath = "/ai";
-const routes = [updateContact, reservationRoutes, addMessageHistory];
+const routes = [reservationRoutes, addMessageHistory];
 
 routes.forEach((_) => {
 	router.use(basePath, _.routes());
