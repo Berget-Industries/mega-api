20b7b475f946a1658961b1b30e2d2ac9a9eb96a4
Denniz Yilmaz <112471865+dennizyilmaz99@users.noreply.github.com>
2023-12-21 14:18:57 +0100

    fixed code for test file

--- src/utils/emailSender.ts
+++ src/utils/emailSender.ts
@@ -4,32 +4,41 @@ import { createJwtToken } from "./jwt.ts";
 import { SMTPClient } from "https://deno.land/x/denomailer/mod.ts";
 
 async function sendMail(to: string, subject: string, content: string) {
+	const username = Deno.env.get("IMAP_USERNAME");
+	const password = Deno.env.get("IMAP_PASSWORD");
+
+	if (!username || !password) {
+		throw new Error("IMAP_USERNAME, IMAP_PASSWORD not set.");
+	}
+
 	const client = new SMTPClient({
 		connection: {
 			hostname: "smtp.gmail.com",
 			port: 465,
 			tls: true,
 			auth: {
-				username: Deno.env.get("IMAP_USERNAME"),
-				password: Deno.env.get("IMAP_PASSWORD"),
+				username,
+				password,
 			},
 		},
 	});
 
 	await client.send({
-		from: Deno.env.get("IMAP_USERNAME"),
+		from: username,
 		to,
 		subject,
 		content,
 	});
 	await client.close();
 }
-
-export const sendResetPasswordMail = async (email: string): Promise<void> => {
-	const token = await createJwtToken({ data: email, type: "reset-password" });
+// I din emailSender.ts
+export const sendResetPasswordMail = async (
+	email: string,
+	dependencies = { createJwtToken, generateTemplate, sendMail }
+): Promise<void> => {
+	const token = await dependencies.createJwtToken({ data: email, type: "reset-password" });
 	const subject = "Återställ lösenord";
+	const content = dependencies.generateTemplate(token);
 
-	const content = generateTemplate(token);
-
-	await sendMail(email, subject, content);
+	await dependencies.sendMail(email, subject, content);
 };
