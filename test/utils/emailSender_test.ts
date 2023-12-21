7263b277101358a920f546e89874b946a19e1e46
Denniz Yilmaz <112471865+dennizyilmaz99@users.noreply.github.com>
2023-12-21 14:14:34 +0100

    updated code

--- test/utils/emailSender_test.ts
+++ test/utils/emailSender_test.ts
@@ -1,42 +1,26 @@
 // Test för sendResetPasswordMail funktionen
 import { assert, assertEquals } from "https://deno.land/std/testing/asserts.ts";
 import * as sinon from "npm:sinon";
-import { sendResetPasswordMail } from "../../src/utils/emailSender.ts"; // Ersätt med sökvägen till din modul
-import { createJwtToken } from "../../src/utils/jwt.ts";
-import { generateTemplate } from "../../src/utils/generateTemplateUtil.ts";
-import { SMTPClient } from "https://deno.land/x/denomailer/mod.ts";
-
-const sandbox = sinon.createSandbox();
+import { sendResetPasswordMail } from "../../src/utils/emailSender.ts";
 
 Deno.test("sendResetPasswordMail - Sends an email with correct content", async () => {
-	try {
-		// Stubba SMTPClient och dess metoder
-		const sendStub = sandbox.stub(SMTPClient.prototype, "send").resolves();
-		const closeStub = sandbox.stub(SMTPClient.prototype, "close").resolves();
+	const sandbox = sinon.createSandbox();
 
-		// Stubba createJwtToken och generateTemplate
-		const tokenStub = sandbox.stub(createJwtToken, "createJwtToken").resolves("fake_token");
-		const templateStub = sandbox
-			.stub(generateTemplate, "generateTemplate")
-			.returns("email content");
+	try {
+		const createJwtTokenMock = sandbox.stub().resolves("fake_token");
+		const generateTemplateMock = sandbox.stub().returns("email content");
+		const sendMailMock = sandbox.stub().resolves();
 
 		const email = "test@example.com";
-		await sendResetPasswordMail(email);
+		await sendResetPasswordMail(email, {
+			createJwtToken: createJwtTokenMock,
+			generateTemplate: generateTemplateMock,
+			sendMail: sendMailMock,
+		});
 
-		assert(sendStub.calledOnce, "Skicka funktionen ska anropas en gång");
-		assertEquals(sendStub.firstCall.args[0].to, email, "E-postadressen ska vara korrekt");
-		assertEquals(
-			sendStub.firstCall.args[0].subject,
-			"Återställ lösenord",
-			"Ämnet ska vara korrekt"
-		);
-		assertEquals(
-			sendStub.firstCall.args[0].content,
-			"email content",
-			"Innehållet ska vara korrekt"
-		);
-
-		assert(closeStub.calledOnce, "Stäng funktionen ska anropas en gång");
+		assert(sendMailMock.calledOnce, "Skicka funktionen ska ha anropats");
+		assert(createJwtTokenMock.calledOnce, "JWT-token skaparfunktionen ska ha anropats");
+		assert(generateTemplateMock.calledOnce, "E-postmallgenereringsfunktionen ska ha anropats");
 	} finally {
 		sandbox.restore();
 	}
