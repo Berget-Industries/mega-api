ad778834a7831dcfcd614978189970ffbd6b5380
Denniz Yilmaz <112471865+dennizyilmaz99@users.noreply.github.com>
2023-12-21 14:14:53 +0100

    updated code

--- test/utils/initOakApp_test.ts
+++ test/utils/initOakApp_test.ts
@@ -1,37 +1,27 @@
 // Test för initOakApp funktionen
 import * as sinon from "npm:sinon";
-import { initOakApp } from "../../src/utils/initOakApp.ts"; // Ersätt med sökvägen till din modul
-import { Application, Router } from "https://deno.land/x/oak@v12.6.1/mod.ts";
+import { Application } from "https://deno.land/x/oak@v12.6.1/mod.ts";
+import { initOakApp } from "../../src/utils/initOakApp.ts";
 
 let sandbox = sinon.createSandbox();
 
 Deno.test("initOakApp - Application startup handling", async () => {
 	try {
-		// Testfall när en giltig port tillhandahålls
-		const validPort = "3000";
-		sandbox.stub(Deno.env, "get").withArgs("PORT").returns(validPort);
+		// Stubbar Deno.env.get för att returnera en testport
+		sandbox.stub(Deno.env, "get").withArgs("PORT").returns("3000");
+
+		// Stubbar Application.listen
 		const listenStub = sandbox.stub(Application.prototype, "listen").resolves();
 
-		await initOakApp();
+		// Startar initOakApp och hanterar AbortController
+		const controller = await initOakApp();
 		sinon.assert.calledOnce(listenStub);
 		sinon.assert.calledWithMatch(listenStub, { port: 3000 });
 
-		// Återställer stubbarna för nästa test
-		sandbox.restore();
-		sandbox = sinon.createSandbox();
-
-		// Testfall när ingen giltig port tillhandahålls
-		sandbox.stub(Deno.env, "get").withArgs("PORT").returns(undefined);
-		const exitStub = sandbox.stub(Deno, "exit");
-
-		try {
-			await initOakApp();
-		} catch (e) {
-			// Ignorera fel eftersom vi förväntar oss ett avbrott i exekveringen
-		}
-
-		sinon.assert.calledOnce(exitStub);
+		// Avbryter och stänger servern
+		controller.abort();
 	} finally {
+		// Återställer stubbarna
 		sandbox.restore();
 	}
 });
