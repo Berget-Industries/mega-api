9a60ea0d4facad14d78cd547334574a2c5da9480
Denniz Yilmaz <112471865+dennizyilmaz99@users.noreply.github.com>
2023-12-29 15:02:28 +0100

    added a handlerespones

--- src/routes/ping.ts
+++ src/routes/ping.ts
@@ -1,13 +1,12 @@
 import { Router, Context } from "https://deno.land/x/oak/mod.ts";
+import { handleResponseError, handleResponseSuccess } from "../utils/contextHandler.ts";
 
 const router = new Router();
 router.post("/ping", (ctx: Context) => {
 	try {
-		ctx.response.status = 200;
-		ctx.response.body = { status: "success", message: "Online" };
+		handleResponseSuccess(ctx, { status: "success", message: "Online" });
 	} catch (error) {
-		ctx.response.status = 500;
-		ctx.response.body = { status: "internal-error", message: "An error occurred", error };
+		handleResponseError(ctx, { status: "internal-error", message: "An error occurred", error });
 	}
 });
 
