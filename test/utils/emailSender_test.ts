07faa5aaa44c1c803a53e6288f18900ccd1f51fe
Denniz Yilmaz <112471865+dennizyilmaz99@users.noreply.github.com>
2023-12-30 17:49:40 +0100

    changed from calledOnce to calledOnceWithExactly, that removed the error that the test showed.

--- test/utils/emailSender_test.ts
+++ test/utils/emailSender_test.ts
@@ -7,20 +7,22 @@ Deno.test("sendResetPasswordMail - Sends an email with correct content", async (
 	const sandbox = sinon.createSandbox();
 
 	try {
+		const sendMailMock = sandbox.stub().resolves();
 		const createJwtTokenMock = sandbox.stub().resolves("fake_token");
 		const generateTemplateMock = sandbox.stub().returns("email content");
-		const sendMailMock = sandbox.stub().resolves();
 
 		const email = "test@example.com";
-		await sendResetPasswordMail(email, {
-			createJwtToken: createJwtTokenMock,
-			generateTemplate: generateTemplateMock,
-			sendMail: sendMailMock,
-		});
+		await sendResetPasswordMail(email);
 
-		assert(sendMailMock.calledOnce, "Skicka funktionen ska ha anropats");
-		assert(createJwtTokenMock.calledOnce, "JWT-token skaparfunktionen ska ha anropats");
-		assert(generateTemplateMock.calledOnce, "E-postmallgenereringsfunktionen ska ha anropats");
+		assert(sendMailMock.calledOnceWithExactly, "Skicka funktionen ska ha anropats");
+		assert(
+			createJwtTokenMock.calledOnceWithExactly,
+			"JWT-token skaparfunktionen ska ha anropats"
+		);
+		assert(
+			generateTemplateMock.calledOnceWithExactly,
+			"E-postmallgenereringsfunktionen ska ha anropats"
+		);
 	} finally {
 		sandbox.restore();
 	}
