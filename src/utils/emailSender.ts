d112c85c0f4be3e4c1a18654b5a81517d26b9807
Denniz Yilmaz <112471865+dennizyilmaz99@users.noreply.github.com>
2024-01-04 18:36:12 +0100

    changed the code to create function instead of save.

--- src/utils/emailSender.ts
+++ src/utils/emailSender.ts
@@ -1,6 +1,6 @@
 import { generateTemplate } from "./generateTemplateUtil.ts";
 import { createJwtToken } from "./jwt.ts";
-import resetPasswordToken from "../models/resetPasswordToken.ts";
+import ResetPasswordToken from "../models/ResetPasswordToken.ts";
 import { SMTPClient } from "https://deno.land/x/denomailer/mod.ts";
 
 async function sendMail(to: string, subject: string, html: string) {
@@ -36,14 +36,9 @@ export const sendResetPasswordMail = async (email: string): Promise<void> => {
 	const token = await createJwtToken({ data: email, type: "reset-password" });
 	const subject = "Återställ lösenord";
 
-	const resetToken = new resetPasswordToken({
-		email: email,
-		token: token,
-	});
-
-	await resetToken.save();
-
 	const html = generateTemplate(token);
+
+	await ResetPasswordToken.create({ email: email, token: token });
 
 	await sendMail(email, subject, html);
 };
