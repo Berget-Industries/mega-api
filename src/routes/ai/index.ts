c90d2c14b3c2f2b5768d4b264f25b66a1c8daf79
willebergh <willebergh@outlook.com>
2024-01-08 00:13:57 +0100

    added new agent config route to ai index

--- src/routes/ai/index.ts
+++ src/routes/ai/index.ts
@@ -1,10 +1,11 @@
 import { Router } from "https://deno.land/x/oak@v12.6.1/mod.ts";
 import reservationRoutes from "./reservations/index.ts";
 import addMessageHistory from "./addMessageHistory.ts";
+import getAgentConfig from "./getAgentConfig.ts";
 
 const router = new Router();
 const basePath = "/ai";
-const routes = [reservationRoutes, addMessageHistory];
+const routes = [reservationRoutes, addMessageHistory, getAgentConfig];
 
 routes.forEach((_) => {
 	router.use(basePath, _.routes());
