a969bcb525dcd75d95d7400469a233539ae8daad
Denniz Yilmaz <denniz@tidings.se>
2024-01-10 13:53:23 +0100

    forgot to add it in index.ts

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -5,10 +5,11 @@ import aiFunctions from "./ai/index.ts";
 import addSelectedMenu from "./addSelectedMenu.ts";
 import dashboard from "./dashboard/index.ts";
 import updateContact from "./updateContact.ts";
+import config from "./config.ts";
 import { Router } from "https://deno.land/x/oak@v12.6.1/mod.ts";
 
 const router = new Router();
-const routes = [auth, admin, aiFunctions, dashboard, addSelectedMenu, ping, updateContact];
+const routes = [auth, admin, aiFunctions, dashboard, addSelectedMenu, ping, updateContact, config];
 
 routes.forEach((_) => {
 	router.use(_.routes());
