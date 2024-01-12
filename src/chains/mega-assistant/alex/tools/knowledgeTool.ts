5d254b29dc68fe707d16fdf5443ee7cb5205fee6
willebergh <willebergh@outlook.com>
2024-01-12 19:13:59 +0100

    skapade verktyg: knowledge

---
+++ src/chains/mega-assistant/alex/tools/knowledgeTool.ts
@@ -0,0 +1,53 @@
+import { z } from "zod";
+import { MongoClient } from "npm:mongodb";
+import CallbackHandler from "../../../callbackHandler.ts";
+import { OpenAIEmbeddings } from "npm:langchain@^0.0.159/embeddings/openai";
+import { DynamicStructuredTool, StructuredTool } from "npm:langchain@^0.0.159/tools";
+import { MongoDBAtlasVectorSearch } from "npm:langchain@^0.0.159/vectorstores/mongodb_atlas";
+import { CallbackManagerForToolRun } from "npm:langchain@^0.0.159/callbacks";
+
+export type knowledgeToolInput = {
+	query: string;
+	document: string;
+};
+
+export const knowledgeToolInputZod = z.object({
+	query: z.string().describe("Frågan du vill få svar på."),
+	document: z
+		.enum(["Trattorian_Hemsida", "Trattorian_Meny"])
+		.describe("Välj vilket document du vill söka i."),
+});
+
+const runFunction = async (
+	{ query, document }: knowledgeToolInput,
+	_runManager: CallbackManagerForToolRun | undefined
+) => {
+	const client = new MongoClient(Deno.env.get("MONGOOSE_CONNECT_URI") || "");
+	const collection = client.db("agent").collection(document);
+
+	const embeddings = new OpenAIEmbeddings();
+	const vectorStore = new MongoDBAtlasVectorSearch(embeddings, {
+		embeddingKey: "embedding", // The name of the collection field containing the embedded text. Defaults to "embedding"
+		indexName: document, // The name of the Atlas search index. Defaults to "default"
+		textKey: "text", // The name of the collection field containing the raw content. Defaults to "text"
+		collection,
+	});
+
+	const result = await vectorStore.similaritySearch(query, 1);
+	const returnValue = result.map((x) => x.pageContent);
+
+	return Promise.resolve(returnValue.join("\n"));
+};
+
+export default function knowledgeTool({ tags }: { tags: string[] }): StructuredTool {
+	return new DynamicStructuredTool({
+		verbose: false,
+		schema: knowledgeToolInputZod,
+		name: "trattorian-knowledge",
+		description:
+			"användbart när du behöver veta något om trattorian. Här finns all din kunskap om trattorian",
+		func: runFunction,
+		callbacks: [new CallbackHandler()],
+		tags,
+	});
+}
