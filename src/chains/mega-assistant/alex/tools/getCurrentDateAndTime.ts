672991f6fbbd9c96a508537a438efd0c55194570
Denniz Yilmaz <denniz@tidings.se>
2024-01-12 19:58:12 +0100

    made changes that berget suggested

--- src/chains/mega-assistant/alex/tools/getCurrentDateAndTime.ts
+++ src/chains/mega-assistant/alex/tools/getCurrentDateAndTime.ts
@@ -1,6 +1,6 @@
 import { CallbackManagerForToolRun } from "npm:langchain@^0.0.159/callbacks";
 import { DynamicTool, StructuredTool } from "npm:langchain@^0.0.159/tools";
-import CallbackHandler from "../../../../../callbackHandler.ts";
+import CallbackHandler from "../../../callbackHandler.ts";
 import moment from "npm:moment";
 
 const runFunction = async (input: string, _runManager: CallbackManagerForToolRun | undefined) => {
@@ -16,8 +16,8 @@ const runFunction = async (input: string, _runManager: CallbackManagerForToolRun
 	);
 };
 
-export const getCurrentDateAndTimeTool = ({ tags }: { tags: string[] }): StructuredTool =>
-	new DynamicTool({
+export default function getCurrentDateAndTimeTool({ tags }: { tags: string[] }): StructuredTool {
+	return new DynamicTool({
 		verbose: false,
 		name: "get-current-date-and-time",
 		description: `användbart när du vill veta vad det är för datum idag eller vad klockan är just nu.
@@ -28,5 +28,4 @@ export const getCurrentDateAndTimeTool = ({ tags }: { tags: string[] }): Structu
 		tags,
 		callbacks: [new CallbackHandler()],
 	});
-
-export default getCurrentDateAndTimeTool;
+}
