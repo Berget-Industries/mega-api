f690ecb49b8d600870ca245386ec198ef5245de4
willebergh <willebergh@outlook.com>
2024-01-13 18:06:36 +0100

    skapde actionCounter för Alex. finns fortfarande lite problem, får lösa senare

---
+++ src/chains/callbackHandlers/actionCounter.ts
@@ -0,0 +1,79 @@
+import { Serialized } from "npm:langchain@^0.0.159/load/serializable";
+import { BaseCallbackHandler } from "npm:langchain@^0.0.159/callbacks";
+import { AgentAction, AgentFinish, ChainValues } from "npm:langchain@^0.0.159/schema";
+
+type onAction = (some: any) => void;
+
+export default class ActionCounterCallbackHandler extends BaseCallbackHandler {
+	name = "ActionCounterCallbackHandler";
+	actions: Record<
+		string,
+		{
+			type: "skapa-reservation" | "redigera-reservation" | "avboka-reservation";
+			docId: string;
+			date: Date;
+			input: string;
+		}
+	> = {};
+
+	onAction: onAction;
+
+	constructor(onAction: onAction) {
+		super();
+		this.onAction = onAction;
+	}
+
+	handleAgentAction(
+		action: AgentAction,
+		runId: string,
+		parentRunId?: string | undefined,
+		tags?: string[] | undefined
+	): void | Promise<void> {
+		const type = action.tool;
+
+		if (
+			type === "skapa-reservation" ||
+			type === "redigera-reservation" ||
+			type === "avboka-reservation"
+		) {
+			console.log(type);
+			this.actions[runId] = { ...this.actions[runId], type };
+		}
+	}
+
+	handleToolStart(
+		tool: Serialized,
+		input: string,
+		runId: string,
+		parentRunId?: string | undefined,
+		tags?: string[] | undefined,
+		metadata?: Record<string, unknown> | undefined
+	) {
+		if (parentRunId && Object.keys(this.actions).includes(parentRunId)) {
+			this.actions[parentRunId] = {
+				...this.actions[parentRunId],
+				input,
+			};
+		}
+	}
+
+	handleToolEnd(
+		output: string,
+		runId: string,
+		parentRunId?: string | undefined,
+		tags?: string[] | undefined
+	) {
+		const res = JSON.parse(output);
+		if (parentRunId && Object.keys(this.actions).includes(parentRunId)) {
+			if (res.status === "success") {
+				this.actions[parentRunId] = {
+					...this.actions[parentRunId],
+					docId: res.reservationData._id,
+					date: new Date(),
+				};
+			} else {
+				delete this.actions[parentRunId];
+			}
+		}
+	}
+}
