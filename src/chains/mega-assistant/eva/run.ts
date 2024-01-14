3963ff5a8b216df5bb37234d9f469b90c0c3c111
willebergh <willebergh@outlook.com>
2024-01-14 14:19:38 +0100

    glömde lägga till denna

--- src/chains/mega-assistant/eva/run.ts
+++ src/chains/mega-assistant/eva/run.ts
@@ -56,6 +56,7 @@ export default async function runEva({
 	const endTime = Date.now();
 
 	const responseTime = endTime - startTime;
+	const usedTokens = tokenCounter.getCount();
 
 	return Promise.resolve({
 		responseTime,
