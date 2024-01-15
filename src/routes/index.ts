9a2fc9600b51a9411cb090d54833492e01b51d71
willebergh <willebergh@outlook.com>
2024-01-15 18:04:25 +0100

    la till alla chains i main routern

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,6 +1,7 @@
 import auth from "./auth/index.ts";
 import admin from "./admin/index.ts";
 import ping from "./ping.ts";
+import chains from "./chains/index";
 import aiFunctions from "./ai/index.ts";
 import addSelectedMenu from "./addSelectedMenu.ts";
 import dashboard from "./dashboard/index.ts";
@@ -8,7 +9,7 @@ import updateContact from "./updateContact.ts";
 import { Router } from "https://deno.land/x/oak@v12.6.1/mod.ts";
 
 const router = new Router();
-const routes = [auth, admin, aiFunctions, dashboard, addSelectedMenu, ping, updateContact];
+const routes = [auth, admin, aiFunctions, dashboard, addSelectedMenu, ping, updateContact, chains];
 
 routes.forEach((_) => {
 	router.use(_.routes());
