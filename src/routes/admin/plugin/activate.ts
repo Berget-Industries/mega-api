18a1eb8efb30692cbd194bad7fb9b6860fdad43c
Denniz Yilmaz <denniz@tidings.se>
2024-01-17 15:00:12 +0100

    created new route for activating plugin

---
+++ src/routes/admin/plugin/activate.ts
@@ -0,0 +1,59 @@
+import { Plugin } from "../../../models/index.ts";
+import { Context, Router } from "https://deno.land/x/oak@v12.6.1/mod.ts";
+import authenticationMiddleware from "../../../middleware/authenticationMiddleware.ts";
+import systemAdminAuthenticationMiddleware from "../../../middleware/systemAdminAuthenticationMiddleware.ts";
+import {
+	handleResponseError,
+	handleResponseSuccess,
+	handleResponsePartialContent,
+} from "../../../utils/contextHandler.ts";
+
+const router = new Router();
+router.post(
+	"/activate/:pluginId",
+	authenticationMiddleware,
+	systemAdminAuthenticationMiddleware,
+	async (ctx: Context) => {
+		try {
+			const { organizationId, name } = await ctx.request.body().value;
+			if (!organizationId || !name) {
+				handleResponsePartialContent(ctx, {
+					status: "missing-information",
+					message:
+						"Saknar någon av dessa nycklar: organizationId, name. Kan inte aktivera plugin.",
+				});
+				return;
+			}
+
+			const foundPlugin = await Plugin.findOne({
+				organization: organizationId,
+				name,
+			});
+			if (!foundPlugin) {
+				handleResponsePartialContent(ctx, {
+					status: "not-found",
+					message:
+						"Detta plugin existerar inte på denna organization. Kunde inte ta bort.",
+				});
+				return;
+			}
+
+			foundPlugin.isActivated = true;
+			await foundPlugin.save();
+
+			handleResponseSuccess(ctx, {
+				status: "success",
+				message: "Lyckades aktivera ett nytt plugin.",
+				plugin: foundPlugin.toObject(),
+			});
+		} catch (error) {
+			console.error(error);
+			handleResponseError(ctx, {
+				status: "internal-error",
+				message: "Ett internt fel har uppstått.",
+			});
+		}
+	}
+);
+
+export default router;
