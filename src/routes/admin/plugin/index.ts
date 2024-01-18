d5db6b91ad4cc6028db7a007019a061a9941d4b3
willebergh <willebergh@outlook.com>
2024-01-18 14:24:46 +0100

    skapade ny route och la till i index. la till andra routes i index också

--- src/routes/admin/plugin/index.ts
+++ src/routes/admin/plugin/index.ts
@@ -1,10 +1,13 @@
 import { Router } from "https://deno.land/x/oak@v12.6.1/mod.ts";
 import add from "./add.ts";
+import list from "./list.ts";
 import remove from "./remove.ts";
+import activate from "./activate.ts";
+import deactivate from "./deactivate.ts";
 
 const router = new Router();
 const basePath = "/plugin";
-const routes = [add, remove];
+const routes = [add, list, remove, activate, deactivate];
 
 routes.forEach((_) => {
 	router.use(basePath, _.routes());
