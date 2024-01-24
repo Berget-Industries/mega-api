bd48d2a02775e5a336016e579c1f78cca4cb0253
willebergh <willebergh@outlook.com>
2024-01-24 14:51:13 +0100

    la till base route

--- src/routes/plugins/index.ts
+++ src/routes/plugins/index.ts
@@ -2,11 +2,12 @@ import addSelectedMenu from "./addSelectedMenu.ts";
 import { Router } from "https://deno.land/x/oak@v12.6.1/mod.ts";
 
 const router = new Router();
+const basePath = "/chains";
 const routes = [addSelectedMenu];
 
 routes.forEach((_) => {
-	router.use(_.routes());
-	router.use(_.allowedMethods());
+	router.use(basePath, _.routes());
+	router.use(basePath, _.allowedMethods());
 });
 
 export default router;
