734702be23a9803acaa4514e5842cc718589f188
willebergh <willebergh@outlook.com>
2024-01-25 13:06:09 +0100

    la till getConfig route i index

--- src/routes/plugins/index.ts
+++ src/routes/plugins/index.ts
@@ -1,9 +1,10 @@
+import getConfig from "./getConfig.ts";
 import addSelectedMenu from "./addSelectedMenu.ts";
 import { Router } from "https://deno.land/x/oak@v12.6.1/mod.ts";
 
 const router = new Router();
-const basePath = "/chains";
-const routes = [addSelectedMenu];
+const basePath = "/plugins";
+const routes = [getConfig, addSelectedMenu];
 
 routes.forEach((_) => {
 	router.use(basePath, _.routes());
