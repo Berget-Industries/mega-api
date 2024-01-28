c89d96f4f1a91656f49fef4bfd4e7cb96a17a534
willebergh <willebergh@outlook.com>
2024-01-28 12:57:38 +0100

    added auth

--- src/utils/initSocketIo.ts
+++ src/utils/initSocketIo.ts
@@ -1,11 +1,19 @@
-import { serve } from "https://deno.land/std@0.166.0/http/server.ts";
-import { Application } from "https://deno.land/x/oak@v12.6.1/mod.ts";
 import { Server } from "https://deno.land/x/socket_io@0.2.0/mod.ts";
+import systemApiKeyWebsocketMiddleware from "../middleware/systemApiKeyWebsocketMiddleware.ts";
 import { handleSocket } from "../websockets/index.ts";
 
 export default function initSocketIo() {
 	const io = new Server({
 		path: "/api/ws/",
+		allowRequest: async (req) => {
+			try {
+				const token = req.headers.get("Authorization");
+				await systemApiKeyWebsocketMiddleware(token);
+				return Promise.resolve();
+			} catch (error) {
+				return Promise.reject("Unauthorized");
+			}
+		},
 	});
 
 	io.on("connection", (socket) => {
