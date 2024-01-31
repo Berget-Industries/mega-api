4b0f65a1ea4f3ed6ee2ccb320e6355be895789a0
willebergh <willebergh@outlook.com>
2024-01-31 11:41:58 +0100

    skapde route för att skapa ny organization

--- src/routes/admin/index.ts
+++ src/routes/admin/index.ts
@@ -1,12 +1,12 @@
 import { Router } from "https://deno.land/x/oak@v12.6.1/mod.ts";
 import createNewUser from "./createNewUser.ts";
-import createNewOrganization from "./createNewOrganization.ts";
+import organization from "./organization/index.ts";
 import createNewApiKey from "./createNewApiKey.ts";
 import plugin from "./plugin/index.ts";
 
 const router = new Router();
 const basePath = "/admin";
-const routes = [createNewUser, createNewOrganization, createNewApiKey, plugin];
+const routes = [createNewUser, organization, createNewApiKey, plugin];
 
 routes.forEach((_) => {
 	router.use(basePath, _.routes());
