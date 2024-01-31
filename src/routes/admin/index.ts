3f0b32f89e9e785d7a2aafafbf746601c4081748
willebergh <willebergh@outlook.com>
2024-01-31 12:44:12 +0100

    skapde route för att skapa api keys

--- src/routes/admin/index.ts
+++ src/routes/admin/index.ts
@@ -1,12 +1,12 @@
 import { Router } from "https://deno.land/x/oak@v12.6.1/mod.ts";
 import createNewUser from "./createNewUser.ts";
 import createNewOrganization from "./createNewOrganization.ts";
-import createNewApiKey from "./createNewApiKey.ts";
+import apiKey from "./apikey/index.ts";
 import plugin from "./plugin/index.ts";
 
 const router = new Router();
 const basePath = "/admin";
-const routes = [createNewUser, createNewOrganization, createNewApiKey, plugin];
+const routes = [createNewUser, createNewOrganization, apiKey, plugin];
 
 routes.forEach((_) => {
 	router.use(basePath, _.routes());
