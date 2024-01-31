88edd4f47a3d33afd8fd4ab3a27ad965398cf02f
willebergh <willebergh@outlook.com>
2024-01-31 11:36:23 +0100

    skapde route för att lista alla användare

--- src/routes/admin/user/index.ts
+++ src/routes/admin/user/index.ts
@@ -1,9 +1,10 @@
 import { Router } from "https://deno.land/x/oak@v12.6.1/mod.ts";
 import create from "./create.ts";
+import list from "./list.ts";
 
 const router = new Router();
 const basePath = "/user";
-const routes = [create];
+const routes = [create, list];
 
 routes.forEach((_) => {
 	router.use(basePath, _.routes());
---
+++ src/routes/admin/user/list.ts
@@ -0,0 +1,31 @@
+import { User } from "../../../models/index.ts";
+import { Context, Router } from "https://deno.land/x/oak@v12.6.1/mod.ts";
+import authenticationMiddleware from "../../../middleware/authenticationMiddleware.ts";
+import systemAdminAuthenticationMiddleware from "../../../middleware/systemAdminAuthenticationMiddleware.ts";
+import { handleResponseError, handleResponseSuccess } from "../../../utils/contextHandler.ts";
+
+const router = new Router();
+router.post(
+	"/list",
+	authenticationMiddleware,
+	systemAdminAuthenticationMiddleware,
+	async (ctx: Context) => {
+		try {
+			const users = await User.find();
+			const usersArray = users.map((user) => user.toObject());
+			handleResponseSuccess(ctx, {
+				status: "success",
+				message: "Lyckades hämta användare.",
+				users: usersArray,
+			});
+		} catch (error) {
+			console.error(error);
+			handleResponseError(ctx, {
+				status: "internal-error",
+				message: "Ett internt fel har uppstått.",
+			});
+		}
+	}
+);
+
+export default router;
