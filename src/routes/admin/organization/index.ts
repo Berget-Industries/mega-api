73ca5a426a54ed60f44dd0c51dc593142e7e210f
willebergh <willebergh@outlook.com>
2024-02-04 17:12:12 +0100

    skapade route för att exportera en hel organisation

--- src/routes/admin/organization/index.ts
+++ src/routes/admin/organization/index.ts
@@ -1,10 +1,11 @@
 import { Router } from "https://deno.land/x/oak@v12.6.1/mod.ts";
 import create from "./create.ts";
+import exportRoute from "./export.ts";
 import list from "./list.ts";
 
 const router = new Router();
 const basePath = "/organization";
-const routes = [create, list];
+const routes = [create, exportRoute, list];
 
 routes.forEach((_) => {
 	router.use(basePath, _.routes());
