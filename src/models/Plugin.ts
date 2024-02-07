477ba9bc2ee2df8ae0524963bdd6456ef1d82d22
willebergh <willebergh@outlook.com>
2024-02-07 09:57:23 +0100

    fixade allt

--- src/models/Plugin.ts
+++ src/models/Plugin.ts
@@ -3,7 +3,7 @@ import { model, Schema, Types } from "npm:mongoose";
 export interface IPlugin {
 	name: string;
 	type: "chain" | "tool" | "input";
-	isActivated: boolean;
+	isActivated?: boolean;
 	config: object;
 	organization: Types.ObjectId;
 	dependencies: string[];
@@ -15,7 +15,7 @@ export default model<IPlugin>(
 	new Schema({
 		name: String,
 		type: String,
-		isActivated: { type: Boolean, default: false },
+		isActivated: { type: Boolean, default: false, required: false },
 		config: Object,
 		organization: { type: Types.ObjectId, ref: "Organization" },
 		dependencies: Array,
