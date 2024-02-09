513d6dd030d8a90df7a20819484b82796825ec5d
willebergh <willebergh@outlook.com>
2024-02-09 12:33:57 +0100

    fixade dessa routes för systemAdmin

--- src/routes/auth/me.ts
+++ src/routes/auth/me.ts
@@ -1,17 +1,38 @@
 import authenticationMiddleware from "../../middleware/authenticationMiddleware.ts";
 import { Router, Context } from "https://deno.land/x/oak@v12.6.1/mod.ts";
 import { handleResponseError, handleResponseSuccess } from "../../utils/contextHandler.ts";
-import User from "../../models/User.ts";
+import { Organization, User } from "../../models/index.ts";
 
 const router = new Router();
 router.get("/me", authenticationMiddleware, async (ctx: Context) => {
-	const user = await User.findById(ctx.state.session.user._id).populate({
-		path: "organizations",
-		select: "-users -__v",
-	});
+	let user = await User.findById(ctx.state.session.user._id)
+		.populate({
+			path: "organizations",
+			select: "-users -__v",
+		})
+		.exec();
+
 	if (!user) {
-		return null;
+		return handleResponseError(ctx, {
+			status: "error",
+			message: "Kunde inte hitta användare.",
+		});
 	}
+
+	if (user.systemAdmin) {
+		const allOrganizations = await Organization.find({});
+		const organizations = allOrganizations.map((_) => _.toObject());
+		const adminUser = { ...user.toObject(), organizations };
+
+		handleResponseSuccess(ctx, {
+			status: "success",
+			message: "Lyckades hämta nuvarande användare.",
+			user: adminUser,
+		});
+
+		return;
+	}
+
 	handleResponseSuccess(ctx, {
 		status: "success",
 		message: "Lyckades hämta nuvarande användare.",
