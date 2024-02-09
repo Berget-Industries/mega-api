d5e23721148f0dfdca5940ed1d60267c00feafe4
willebergh <willebergh@outlook.com>
2024-02-09 14:51:43 +0100

    skapde två routes för att hantera användares access till organisationer.

--- src/routes/admin/user/index.ts
+++ src/routes/admin/user/index.ts
@@ -2,10 +2,12 @@ import { Router } from "https://deno.land/x/oak@v12.6.1/mod.ts";
 import list from "./list.ts";
 import remove from "./remove.ts";
 import create from "./create.ts";
+import addOrganization from "./addOrganization.ts";
+import removeOrganization from "./removeOrganization.ts";
 
 const router = new Router();
 const basePath = "/user";
-const routes = [create, remove, list];
+const routes = [create, remove, list, addOrganization, removeOrganization];
 
 routes.forEach((_) => {
 	router.use(basePath, _.routes());
