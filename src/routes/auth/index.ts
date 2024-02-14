606efbcfb77192e6226c3cdfce553dd8b2a8d36f
willebergh <willebergh@outlook.com>
2024-02-14 13:33:55 +0100

    added to index

--- src/routes/auth/index.ts
+++ src/routes/auth/index.ts
@@ -1,13 +1,22 @@
 import { Router } from "https://deno.land/x/oak@v12.6.1/mod.ts";
-import resetPasswordWithToken from "./resetPasswordWithToken.ts";
+
+import me from "./me.ts";
 import login from "./login.ts";
 import logout from "./logout.ts";
-import me from "./me.ts";
+import resetPasswordWithToken from "./resetPasswordWithToken.ts";
+import checkResetPasswordToken from "./checkResetPasswordToken.ts";
 import requestResetPasswordToken from "./requestResetPasswordToken.ts";
 
 const router = new Router();
 const basePath = "/auth";
-const routes = [resetPasswordWithToken, login, logout, me, requestResetPasswordToken];
+const routes = [
+	me,
+	login,
+	logout,
+	resetPasswordWithToken,
+	checkResetPasswordToken,
+	requestResetPasswordToken,
+];
 
 routes.forEach((_) => {
 	router.use(basePath, _.routes());
