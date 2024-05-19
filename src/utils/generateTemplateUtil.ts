cd5f949d1f3c568f843381ff941d4a2018c45b9d
willebergh <willebergh@outlook.com>
2024-05-19 17:01:07 +0200

    fixade länk i mail & env var i hela cicd flöde

--- src/utils/generateTemplateUtil.ts
+++ src/utils/generateTemplateUtil.ts
@@ -1,12 +1,15 @@
 const getServerHostUrl = () => {
-	let serverType = "dev";
-
 	const megaEnv = Deno.env.get("MEGA_ENV");
+
 	if (megaEnv === "production") {
-		serverType = "production";
+		return "https://twostack-system.se";
 	}
 
-	return `https://mega-dashboard.${serverType}.berget.industries/auth/jwt/register`;
+	if (megaEnv === "stage") {
+		return `https://dashboard.tss.stage.berget.industries`;
+	}
+
+	return "http://localhost:3000";
 };
 
 export function generateTemplate(token: string) {
@@ -14,7 +17,7 @@ export function generateTemplate(token: string) {
     <div style="font-family: Arial, sans-serif; text-align: center;">
         <h1>Återställ lösenord</h1>
         <p>Det verkar som att du har begärt en återställning av ditt lösenord. Du kan enkelt skapa ett nytt lösenord genom att klicka på länken nedan.</p>
-        <a href="${getServerHostUrl()}?token=${token}" style="display: inline-block; padding: 10px 20px; margin: 20px 0; background-color: #007bff; color: white; text-decoration: none; border-radius: 5px;">Återställ lösenord</a>
+        <a href="${getServerHostUrl()}/auth/jwt/register?token=${token}" style="display: inline-block; padding: 10px 20px; margin: 20px 0; background-color: #007bff; color: white; text-decoration: none; border-radius: 5px;">Återställ lösenord</a>
         <p>Om du inte begärde en återställning, kan du ignorera detta meddelande.</p>
     </div>
     `;
